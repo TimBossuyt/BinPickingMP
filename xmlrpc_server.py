d178c6cd685bdb185700f6136a3f48ee3b8eaf64
Tim Bossuyt <tim.bossuyt@ugent.be>
2025-02-15 12:16:33 +0100

    Improve error handling and logging throughout XML-RPC server

--- xmlrpc_server.py
+++ xmlrpc_server.py
@@ -92,8 +92,13 @@ class RpcServer:
 
         :return: None
         """
-        logger.debug(f"Starting XML-RPC server on {self.host}:{self.port}")
-        self.thrServer.start()
+        logger.info(f"Initializing XML-RPC server on {self.host}:{self.port}")
+
+        try:
+            self.thrServer.start()
+            logger.info("XML-RPC server thread started successfully")
+        except Exception as e:
+            logger.error(f"Failed to start XML-RPC server: {str(e)}")
 
     def __run(self):
         """
@@ -104,8 +109,12 @@ class RpcServer:
 
         :return: None
         """
-        logger.info("Started XML-RPC server")
-        self.server.serve_forever()
+        logger.info(f"Server is running on {self.host}:{self.port}")
+
+        try:
+            self.server.serve_forever()
+        except Exception as e:
+            logger.error(f"Error in the server loop: {str(e)}")
 
     def Stop(self):
         """
@@ -115,21 +124,26 @@ class RpcServer:
         """
 
         logger.info("Stopping XML-RPC server")
-        ## Shutdown camera thread / connection
-        self.oCamera.Disconnect()
+        try:
+            ## Shutdown camera thread / connection
+            self.oCamera.Disconnect()
+            logger.debug("Camera disconnected succesfully")
 
-        ## Stops .server_forever loop and finish all pending requests, does not close socket
-        self.server.shutdown()
+            ## Stops .server_forever loop and finish all pending requests, does not close socket
+            self.server.shutdown()
 
-        ## Safely close the socket
-        self.server.server_close()
+            ## Safely close the socket
+            self.server.server_close()
+            logger.info("Server socket closed")
 
-        ## If thread still alive --> wait for it to stop
-        if self.thrServer.is_alive():
-            logger.debug("Waiting for server thread to finish")
-            self.thrServer.join()
+            ## If thread still alive --> wait for it to stop
+            if self.thrServer.is_alive():
+                logger.debug("Waiting for server thread to finish")
+                self.thrServer.join()
 
-        logger.info("Stopped XML-RPC server")
+            logger.info("Server thread succesfully terminated")
+        except Exception as e:
+            logger.error(f"An error occurred while stopping the server: {str(e)}")
 
 
     def register_methods(self):
@@ -174,35 +188,54 @@ class RpcServer:
         """
 
         logger.debug("Received getConnectedDevices call")
-        return getConnectedDevices()
+        try:
+            devices = getConnectedDevices()
+            logger.debug(f"Connected devices: {devices}")
+
+            return devices
+        except Exception as e:
+            logger.error(f"Error while fetching connecting devices: {str(e)}")
+
 
     def getCameraPreview(self):
         """
-        Retrieves the current camera preview frame as a base64 encoded image.
-        If the camera is connected, it fetches the live feed.
+        Fetches a preview frame from the connected camera or a placeholder image
+        if the camera is not connected.
 
-        If the camera is not connected, it displays a default "no-camera" image.
+        If the camera is connected and functional, it retrieves a video frame, encodes it as a JPEG image,
+        and returns the encoded frame as a base64 string. If the camera is not connected or a frame is not
+        available, a predefined placeholder image ('no-camera.jpg') is used instead, which is also encoded
+        and returned as a base64 string.
 
-        :return: Base64 encoded image of the camera preview or a default image based on the camera's connection status.
+        :return: Base64-encoded JPEG image of the camera preview or a placeholder image.
+        :rtype: bytes
         """
 
         ## TODO: Check for problems when not connected and cvFrame is null
 
         # logger.debug("Received getCameraPreview call")
-        if self.oCamera.bConnected:
-            cvFrame = self.oCamera.getCvVideoPreview()
+        try:
+            if self.oCamera.bConnected:
+                cvFrame = self.oCamera.getCvVideoPreview()
 
-            if cvFrame is not None:
+                if cvFrame is not None:
+                    _, buff = cv2.imencode('.jpg', cvFrame)
+                    enc_data = base64.b64encode(buff)
+                    return enc_data
+                else:
+                    logger.warning("No frame available from connected camera")
+
+            else:
+                logger.warning("Camera is not connected, returning no-camera.jpg")
+                cvFrame = cv2.imread("no-camera.jpg")
                 _, buff = cv2.imencode('.jpg', cvFrame)
 
                 enc_data = base64.b64encode(buff)
                 return enc_data
-        else:
-            cvFrame = cv2.imread("no-camera.jpg")
-            _, buff = cv2.imencode('.jpg', cvFrame)
 
-            enc_data = base64.b64encode(buff)
-            return enc_data
+        except Exception as e:
+            logger.error(f"Error while fetching camera preview: {str(e)}")
+
 
     def getImageFrame(self):
         """
@@ -213,21 +246,26 @@ class RpcServer:
 
         :return: Base64 encoded string of the image frame.
         """
+        logger.info("Received request for a single camera frame")
 
-        if self.oCamera.bConnected:
-            cvFrame = self.oCamera.getCvImageFrame()
-
-            if cvFrame is not None:
+        try:
+            if self.oCamera.bConnected:
+                logger.debug("Camera is connected, retrieving image frame")
+                cvFrame = self.oCamera.getCvImageFrame()
+                if cvFrame is not None:
+                    _, buff = cv2.imencode('.jpg', cvFrame)
+                    enc_data = base64.b64encode(buff)
+                    return enc_data
+                else:
+                    logger.warning("No image frame received from the connected camera")
+            else:
+                logger.warning("Camera is not connected, returning no-camera.jpg")
+                cvFrame = cv2.imread("no-camera.jpg")
                 _, buff = cv2.imencode('.jpg', cvFrame)
-
                 enc_data = base64.b64encode(buff)
                 return enc_data
-        else:
-            cvFrame = cv2.imread("no-camera.jpg")
-            _, buff = cv2.imencode('.jpg', cvFrame)
-
-            enc_data = base64.b64encode(buff)
-            return enc_data
+        except Exception as e:
+            logger.error(f"Error while fetching camera frame: {str(e)}")
 
 
     def connectCameraByMxId(self, sMxId):
@@ -235,9 +273,13 @@ class RpcServer:
         :param sMxId: The identifier for the camera to connect to.
         :return: Returns 0 upon successful connection of the camera.
         """
-        logger.debug(f"Received connectCameraByMxId ({sMxId}) call")
-        self.oCamera.Connect(sMxId)
-        return 0
+        logger.info(f"Received request to connect camera with MxId: {sMxId}")
+        try:
+            self.oCamera.Connect(sMxId)
+            logger.info(f"Successfully connected to camera with MxId: {sMxId}")
+            return 0
+        except Exception as e:
+            logger.error(f"Error connecting to camera with MxId {sMxId}: {str(e)}")
 
     def runCalibration(self, sWorldPointsJson):
         """
@@ -245,26 +287,33 @@ class RpcServer:
         :return: An integer indicating the status of the calibration process (default: 0).
         """
 
-        logger.debug(f"Received runCalibration call")
+        logger.info("Received request to run camera calibration")
+        try:
+            ## Calibrate camera function expects dictionary --> json to dict
+            dictWorldPoints = self.__deserializeWorldPointsJson(sWorldPointsJson)
+            logger.debug(f"World points for calibration: {dictWorldPoints}")
 
-        ## Calibrate camera function expects dictionary --> json to dict
-        dictWorldPoints = self.__deserializeWorldPointsJson(sWorldPointsJson)
+            self.oCamera.calibrateCamera(dictWorldPoints)
+            logger.info("Camera calibration completed successfully")
 
-        self.oCamera.calibrateCamera(dictWorldPoints)
-
-        return 0
+            return 0
+        except Exception as e:
+            logger.error(f"Error during camera calibration: {str(e)}")
 
     def getCalibrationImage(self):
         """
         :return: Encoded calibration image in Base64 format
         """
-        img = self.oCamera.imgCalibration
 
-        _, buff = cv2.imencode('.jpg', img)
+        logger.info("Received request for calibration image")
+        try:
+            img = self.oCamera.imgCalibration
+            _, buff = cv2.imencode('.jpg', img)
+            enc_data = base64.b64encode(buff)
 
-        enc_data = base64.b64encode(buff)
-
-        return enc_data
+            return enc_data
+        except Exception as e:
+            logger.error(f"Error while retrieving calibration image: {str(e)}")
 
 
     def disconnectCamera(self):
@@ -276,9 +325,15 @@ class RpcServer:
 
         :return: Returns 0 upon successful disconnection.
         """
-        logger.debug(f"Received disconnectCamera call")
-        self.oCamera.Disconnect()
-        return 0
+
+        logger.info("Received request to disconnect camera")
+        try:
+            self.oCamera.Disconnect()
+            logger.info("Camera disconnected successfully")
+
+            return 0
+        except Exception as e:
+            logger.error(f"Error while disconnecting camera: {str(e)}")
 
     ################## UTILS ##################
     def __deserializeWorldPointsJson(self, dict_serialized):
@@ -286,8 +341,14 @@ class RpcServer:
         :param dict_serialized: A JSON string representing a dictionary where keys are expected to be numeric (as strings) and values are associated data.
         :return: A dictionary with keys converted to integers and values preserved from the input JSON string.
         """
-        dict_world_points = json.loads(dict_serialized)
+        logger.debug(f"Deserializing world points JSON: {dict_serialized}")
 
-        dict_world_points = {int(k): v for k, v in dict_world_points.items()}
+        try:
+            dict_world_points = json.loads(dict_serialized)
 
-        return dict_world_points
+            dict_world_points = {int(k): v for k, v in dict_world_points.items()}
+
+            logger.debug(f"Deserialized world points: {dict_world_points}")
+            return dict_world_points
+        except Exception as e:
+            logger.error(f"Error deserializing world points JSON: {str(e)}")
